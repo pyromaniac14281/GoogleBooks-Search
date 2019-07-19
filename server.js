960b962dd2b691a2235e9d84025a39113890d05b
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-19 14:04:05 -0400

    server.js in process of creation

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
