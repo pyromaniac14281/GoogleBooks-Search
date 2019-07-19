0356491b7d8f80f830b877287df34a8a3d4921c1
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-07-19 14:05:10 -0400

    server.js in process of creation

--- server.js
+++ server.js
@@ -5,3 +5,23 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+const mongoose = require("mongoose");
+const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
+mongoose.connect(mongoURL, {useNewUrlParser: true})
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+
+require("./routes/api-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`world ==> API server now on port ${PORT}!`);
+});
